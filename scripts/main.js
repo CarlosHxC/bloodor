ca931b5d017a6a0d965aa6acc939c9787f1d0364
Carlos García <carlos.garcia.92@hotmail.com>
2015-04-28 17:53:45 -0600

    bloodor primer commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+   /*Disque scroll 
+   REVISAR
+   */ $('a[href*=#]').bind('click',function(event){
+        var $anchor = $(this);
+ 
+        /*
+        if you don't want to use the easing effects:
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        */
+    $('html, body').stop().animate({
+           scrollTop:           $($anchor.attr('href')).offset().top
+        }, 2000,'easeInOutExpo');
+   
+        event.preventDefault();
+    });
+    
+    $('.frase1').flowtype({
+        minimum: 500,
+        maximum: 1200,
+        minFont: 14,
+        maxFont: 35,
+        fontRatio: 30
+    )};
+});
