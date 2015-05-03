7dec966d4da3e98027fcc37ce068d9fe333934e0
Carlos García <carlos.garcia.92@hotmail.com>
2015-05-02 20:03:54 -0600

    selectores

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
    /*Disque scroll 
    REVISAR
    */ $('a[href*=#]').bind('click',function(event){
@@ -10,8 +11,8 @@ $(document).ready(function() {
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1000);
         */
-    $('html, body').stop().animate({
-           scrollTop:           $($anchor.attr('href')).offset().top
+        $('html, body').stop().animate({
+           scrollTop: $($anchor.attr('href')).offset().top
         }, 2000,'easeInOutExpo');
    
         event.preventDefault();
@@ -23,5 +24,16 @@ $(document).ready(function() {
         minFont: 14,
         maxFont: 35,
         fontRatio: 30
-    )};
+    });
+    
+   $('.sel-form-sangre').click( function () {
+      $('.active').removeClass('active');
+      $(this).addClass('active');
+    });
+    
+     $('.form-sel-button').click( function () {
+      $('.form-sel-button').removeClass('active-reg');
+      $(this).addClass('active-reg');
+    });
+    
 });
